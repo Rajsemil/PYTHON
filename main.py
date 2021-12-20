68170789e3a241eefdfe15fad17eb1dade8c6fde
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2021-12-20 16:05:15 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-x = float(input("Enter a number: "))
-if x<-0:
-    print("Negative number")
-elif x==0:
-    print("Equal to zero")
-elif x>1000:
-    print("Greater  number")
-else:
-    print("Small number")
-print("Rest of the code")
+m = lambda x:print(x**2)
+m(int(input("Enter a number: ")))
